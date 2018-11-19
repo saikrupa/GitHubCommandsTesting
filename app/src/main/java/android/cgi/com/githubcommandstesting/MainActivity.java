6cf27bcd7afddd8471b9569388d237297b074d97
sai <puram.saikurpa@gmail.com>
2018-11-19 17:17:00 +0530

    Project pushed

---
+++ app/src/main/java/android/cgi/com/githubcommandstesting/MainActivity.java
@@ -0,0 +1,13 @@
+package android.cgi.com.githubcommandstesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
