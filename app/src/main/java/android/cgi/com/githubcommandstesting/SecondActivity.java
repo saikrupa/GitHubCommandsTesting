8ad7633773620dc7ea3d798028121a725c48cc5a
sai <puram.saikurpa@gmail.com>
2018-11-21 16:37:24 +0530

    second activity changed

--- app/src/main/java/android/cgi/com/githubcommandstesting/SecondActivity.java
+++ app/src/main/java/android/cgi/com/githubcommandstesting/SecondActivity.java
@@ -1,6 +1,12 @@
 package android.cgi.com.githubcommandstesting;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 public class SecondActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
